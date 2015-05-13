aedeb3650da0cd86b3e633d40f9b816f5fdba4e3
Hack Reactor Students <accounts@hackreactor.com>
2015-05-13 15:15:48 -0700

    Complete functional queue.js and stack.js.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,43 @@
 var Queue = function(){
   var someInstance = {};
-
+  var size = 0;
   // Use an object with numeric keys to store values
   var storage = {};
+  var first = 0;
+  var last = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[last] = value;
+    last++;
+    size++;
   };
 
   someInstance.dequeue = function(){
-  };
+    // if (size > 0) {
+    //   var record = storage[Object.keys(storage)[0]];
+    //   delete storage[Object.keys(storage)[0]];
+    //   size--;
+    //   return record;
+    // }
+    if(size > 0){
+      var record = storage[first];
+      delete storage[first];
+      first++;
+      size--;
+      return record;
+    }
+   };
 
   someInstance.size = function(){
+    // return Math.max(0, size)
+    return size;
+
   };
 
   return someInstance;
 };
+
+// 0 : a
+//
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,26 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
   someInstance.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function(){
+    if(size >0){
+      var record = storage[size - 1];
+      delete storage[size - 1];
+      size--;
+      return record;
+    }
+
   };
 
   someInstance.size = function(){
+    // return Math.max(0, size);
+    return size;
   };
 
   return someInstance;
