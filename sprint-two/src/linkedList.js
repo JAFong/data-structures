16aff6c29ad830430135481ab2018554d053e450
Hack Reactor Students <accounts@hackreactor.com>
2015-05-15 12:32:11 -0700

    Passed 5/7 on linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    if (list.tail) {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
+    if (list.head === null) {
+      list.head = newNode;
+    }
   };
 
   list.removeHead = function(){
+    var record = list.head;
+    delete list.head;
+    list.head = record.next;
+    return record.value;
   };
 
   list.contains = function(target){
+    var findNumber = function(node) {
+      if (target === 5) {
+        debugger;
+      }
+      if (node.value === target) {
+        return true;
+      } else if (node.next) {
+        findNumber(node.next);
+      }
+      // return false;
+    };
+    if (findNumber(list.head)) {
+      return findNumber(list.head);
+    }
   };
 
   return list;
