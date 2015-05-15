cb1e52e59c216aa1641fd780af55f4e56f2f2642
Hack Reactor Students <accounts@hackreactor.com>
2015-05-15 15:56:39 -0700

    Complete tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,8 +3,8 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -15,11 +15,39 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var newNode = {};
+  newNode.value = value;
+  newNode.children =  [];
+  _.extend(newNode, treeMethods);
+  this.children.push(newNode);
 };
 
 treeMethods.contains = function(target){
-
+  var result = false;
+  var findNode = function findNode(childArray){
+    for(var child = 0; child < childArray.length; child++){
+      if(childArray[child].value === target){
+        result = true;
+        return;
+      } else if(childArray[child].children){
+        findNode(childArray[child].children);
+      }
+    }
+  };
+  findNode(this.children);
+  return result;
+  // var findNode = function(node) {
+  //   if (node.value === target) {
+  //     hasTarget = true;
+  //     return;
+  //   } else if (node.children.length > 0) {
+  //     _.each(node.children, function(child){
+  //       findNode(child);
+  //     });
+  //   }
+  // };
+  // findNode(this);
+  // return hasTarget;
 };
 
 
